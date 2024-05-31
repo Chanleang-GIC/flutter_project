a3449950c14d7a4b33ad34c8093ab49519c802a7
chanleang <chanleang7779@gmail.com>
2024-05-31 21:05:39 +0700

    Initial commit

---
+++ lib/testscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class testscreen extends StatelessWidget {
+  const testscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 100,
+    );
+  }
+}
